9b049e936ffde2b417baf6d2da799c47b5a4463d
Gemille Ford <gemille.ford@manheim.com>
2016-06-16 17:31:19 -0400

    move constants.rb to resources folder

---
+++ lib/resources/constants.rb
@@ -0,0 +1,6 @@
+
+class Constants
+  SUITS = %w(Hearts, Spades, Clubs, Diamonds).freeze
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'].freeze
+  RANGE_FOR_CARDS = (1..13).freeze
+end
