52dd0bb38ef138ca074e0943b9c2235a504b32cc
Gemille Ford <gemille.ford@manheim.com>
2018-02-06 22:27:35 -0500

    refactor board specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,13 @@ require 'rspec'
 require 'board'
 
 describe Board do
+  before { @board = Board.new }
+
   it 'initializes with an empty card array instance' do
-    board = Board.new
-    expect(board.cards).to eq([])
+    expect(@board.cards).to eq([])
   end
 
   it 'initializes with an instance of pot set to 0' do
-    board = Board.new
-    expect(board.pot).to eq(0)
+    expect(@board.pot).to eq(0)
   end
 end
